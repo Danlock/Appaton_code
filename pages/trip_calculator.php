b39ec47411b9a8746e0100b03dd9235a1cad78b6
jake <jake_6998@hotmail.com>
2014-03-01 12:34:04 -0400

    layout for selections

---
+++ pages/trip_calculator.php
@@ -0,0 +1,97 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Road Trip Planner</title>
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="../js/bootstrap.js"></script>
+	</head>
+	<body style="background-image:url('../images/retro.jpg')">
+		    
+		  
+		  <div class="container" style="background-color:white">
+	         
+	              <ul class="nav nav-pills nav-justified">
+	                <li><a href="#">Home</a></li>
+	                <li ><a href="#">Gas Prices</a></li>            
+	                <li><a href="#">Trip Cost Calculator</a></li>
+	                <li><a href="#">About</a></li>
+	                <li><a href="#">Contact</a></li>
+	              </ul>
+	    
+		</br>
+		</br>
+		
+		<div class="row">
+				<div class="col-md-2 text-center"> Left Arrow </div>
+
+		<div class="col-md-8 text-center"> <img src="../images/truck.png" alt="..." class="img-rounded"> </div>
+		<div class="col-md-2 text-center"> Right arrow </div>
+		</div>
+		
+		</br>
+		<div class="row">
+		
+	         <div class="col-md-5 text-right"> <p style="font-family:Comic Sans MS, cursive, sans-serif;"> Vehicle Class </p> </div>
+		<div class="col-md-3">  <select name="Surf1" style="width: 200px">
+            <option> </option>
+			</select>
+			</div>
+		
+		</div>
+		
+		
+		</br>
+		
+		<div class="row">
+         <div class="col-md-3"> <p style="font-family:Comic Sans MS, cursive, sans-serif;"> Vehicle Brand </p> </div>
+		<div class="col-md-3">  <select name="Surf1" style="width: 200px">
+            <option> </option>
+			</select>
+			</div>
+			
+			 <div class="col-md-3"> <p style="font-family:Comic Sans MS, cursive, sans-serif;"> Engine Size </p> </div>
+		<div class="col-md-3">  <select name="Surf1" style="width: 200px">
+            <option> </option>
+			</select>
+			
+			</div>
+     </div>
+	 
+	 
+	 
+	 		</br>
+		
+		<div class="row">
+         <div class="col-md-3"> <p style="font-family:Comic Sans MS, cursive, sans-serif;"> Option n </p> </div>
+		<div class="col-md-3">  <select name="Surf1" style="width: 200px">
+            <option> </option>
+			</select>
+			</div>
+			
+			 <div class="col-md-3"> <p style="font-family:Comic Sans MS, cursive, sans-serif;"> Option n </p> </div>
+		<div class="col-md-3">  <select name="Surf1" style="width: 200px">
+            <option> </option>
+			</select>
+			
+			</div>
+     </div>
+	 
+</br>
+</br>
+	 <button type="button" class="btn btn-primary col-md-12 text-center">Search</button>
+</br>
+</br>
+</br>
+</br>
+
+		
+	<footer>
+        <p>&copy; carribeanada 2014</p>
+      </footer>
+</div>

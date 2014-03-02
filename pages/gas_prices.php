d5ce03cbf06b5b46faa33c78ae0ac78d7e91a2c1
jake <jake_6998@hotmail.com>
2014-03-02 12:22:53 -0400

    adding FAQS and stats page

--- pages/gas_prices.php
+++ pages/gas_prices.php
@@ -19,9 +19,151 @@
 	                <li><a href="about.php">About</a></li>
 	                <li><a href="contact.php">Contact</a></li>
 	              </ul>
-				  
-				  
-	    
+				
+<div class="row">				
+				<div class="col-md-12" > <h3> Most Fuel Inefficient Vehicle(s) </h3> </div>
+				</div>
+				
+				<div class="row">
+				<div class="col-md-12 text-center">
+				<div class="table-responsive text-left">
+				<table class="table">
+				<tr class="danger">
+				<th>Brand</th>
+				<th>Model</th>
+				<th>Class</th>
+				<th>Fuel (L/Yr)</th>
+				</tr>
+				<tr>
+				<td>CHEVROLET</td>
+				<td>EXPRESS 3500 PASSENGER</td>
+				<td>VAN - PASSENGER</td>
+				<td>4620</td>
+				</tr>
+				<tr>
+				<td>GMC</td>
+				<td>SAVANA 3500 PASSENGER</td>
+				<td>VAN - PASSENGER</td>
+				<td>4620</td>
+				</tr>
+				</table>
+	    </div>
+		</div>
+		</div>
+		</br>
+		
+		
+		
+		<div class="row">				
+				<div class="col-md-12" > <h3> Most Fuel Efficient Vehicle(s) </h3> </div>
+				</div>
+				
+				<div class="row">
+				<div class="col-md-12 text-center">
+				<div class="table-responsive text-left">
+				<table class="table">
+				<tr class="success">
+				<th>Brand</th>
+				<th>Model</th>
+				<th>Class</th>
+				<th>Fuel (L/Yr)</th>
+				</tr>
+				<tr>
+				<td>HONDA</td>
+				<td>ACCORD HYBRID</td>
+				<td>MID-SIZE</td>
+				<td>760</td>
+				</tr>
+				<tr>
+				<td>TOYOTA</td>
+				<td>PRIUS</td>
+				<td>MID-SIZE</td>
+				<td>760</td>
+				</tr>
+				</table>
+	    </div>
+		</div>
+		</div>
+		</br>
+		
+		
+		
+		<div class="row">				
+				<div class="col-md-12" > <h3> Most Environmental Unfriendly Vehicle(s) </h3> </div>
+				</div>
+				
+				<div class="row">
+				<div class="col-md-12 text-center">
+				<div class="table-responsive text-left">
+				<table class="table">
+				<tr class="danger">
+				<th>Brand</th>
+				<th>Model</th>
+				<th>Class</th>
+				<th>co2 Emissions</th>
+				</tr>
+				<tr>
+				<td>MINI</td>
+				<td>JOHN COOPER WORKS COUNTRYMAN ALL4</td>
+				<td>COMPACT</td>
+				<td>3358</td>
+				</tr>
+				<tr>
+				<td>FORD</td>
+				<td>E350 WAGON</td>
+				<td>VAN - PASSENGER</td>
+				<td>435</td>
+				</tr>
+				</table>
+	    </div>
+		</div>
+		</div>
+		</br>
+		
+		
+		
+		
+		<div class="row">				
+				<div class="col-md-12" > <h3> Most Environmental Friendly Vehicle(s) </h3> </div>
+				</div>
+				
+				<div class="row">
+				<div class="col-md-12 text-center">
+				<div class="table-responsive text-left">
+				<table class="table">
+				<tr class="success">
+				<th>Brand</th>
+				<th>Model</th>
+				<th>Class</th>
+				<th>co2 Emissions</th>
+				</tr>
+				<tr>
+				<td>HONDA</td>
+				<td>ACCORD HYBRID</td>
+				<td>MID-SIZE</td>
+				<td>87</td>
+				</tr>
+				<tr>
+				<td>TOYOTA</td>
+				<td>PRIUS</td>
+				<td>MID-SIZE</td>
+				<td>87</td>
+				</tr>
+				</table>
+	    </div>
+		</div>
+		</div>
+		</br>
+		
+		
+		
+		<div class="row">				
+				<div class="col-md-12" > <h3>Graph Showing Most Fuel Efficient Vehicles by Brand</h3> </div>
+				</div>
+								<div class="col-md-12"> <img src="../graphs/fuel_efficiency_by_brand.png" alt="..." class="img-rounded" id="classpic" width="600" height="600"> </div>
+
+				</br>
+		
 	<footer>
         <p>&copy; carribeanada 2014</p>
       </footer>
